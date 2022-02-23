6b2dfe2b03af325e1b8c152d5edea3c953b2a205
theNamanchauhan <99641336+theNamanchauhan@users.noreply.github.com>
2022-02-23 14:17:47 +0530

    Add files via upload

---
+++ Task 4/Problem 1/Problem 1.py
@@ -0,0 +1,6 @@
+p=input("ENTER THE FIRST NUMBER: ")
+p=float(p) #Converting p(string) into a floating point number
+q=input("ENTER THE SECOND NUMBER: ")
+q=float(q) #Converting q(string) into a floating point number
+sum=p+q
+print("SUM OF THE TWO NUMBERS IS: ",sum)
